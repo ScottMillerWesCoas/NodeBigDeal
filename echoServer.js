8f8adbbb9d9dd740ead98cd98635668d3502ac37
Scott Miller <scott.t.miller1@gmail.com>
2016-09-28 11:12:07 -0700

    Update echoServer.js

--- echoServer.js
+++ echoServer.js
@@ -2,7 +2,7 @@ var net = require('net');
 
 /* FOR A LIL' INTER-TERMINAL WINDOW CHAT APP, simply navigate to the folder containing this file in terminal, 
 TYPE node chatAppNode.js (or whatever you renamed this file), then open another terminal window (or 2 to see this thing work)
-and type telnet localhost 3003 (or whatever the server is listening to, listed below on line 38) and type away in either of those
+and type telnet localhost 3020 (or whatever the server is listening to, listed below on line 38) and type away in either of those
 other terminals for chat app magic; 
 */
 
