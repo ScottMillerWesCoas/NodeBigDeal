0026983f11dbba53bceab90a6e8263b8625d1ec1
ScottMillerWesCoas <scott.t.miller1@gmail.com>
2016-10-06 21:02:23 -0700

    express server scrapes websites, writes to new file, serves file to user

--- expressServer.js
+++ expressServer.js
@@ -2,6 +2,9 @@ var express = require('express');
 var fs = require('fs'); 
 var app = express(); 
 var request = require('request');
+var url = require('url');
+
+
 
 app.get('/express', function(req, res){
   res.sendFile(__dirname + '/index.html'); 
@@ -25,13 +28,10 @@ app.listen(5000, function(){
   console.log('listenin on 5000'); 
 }); 
 
-var newFile; 
-//CONSOLE.LOG GOOGLE'S BODY
-
+//Webscraper functionality 
 app.get('/express/ws/:website', function(req, res){
   var ws = req.params.website; 
-  //var ws = website.slice(0, website.length-4); 
-  console.log('WS ' + ws); 
+  //var ws = website.slice(0, website.length-4); FOR ENTERING .com at end of URL
   var goGet = 'http://www.' + ws + '.com'; 
   console.log('GETTING ' + goGet); 
   request(goGet, function (error, response, body) {
@@ -47,6 +47,34 @@ app.get('/express/ws/:website', function(req, res){
   }, 1500); 
 }); 
 
+
+
+app.get('/express/twitter/:user', function(req, res){
+  var user = req.params.user; 
+  var options = {
+    protocol: "https:",
+    host: "api.twitter.com/1.1",
+    pathname: '/search/tweets.json',
+    query: {
+      q: '?q=' + user + '&src=typd'
+    }
+  };
+  console.log('searching twitter for ' + user + ' mentions'); 
+  var searchURL = url.format(options);
+  request(searchURL).pipe(res); 
+}); 
+
+
+
+//  https://twitter.com/search?q=%40twitterapi
+// Replace “https://twitter.com/search” with “https://api.twitter.com/1.1/search/tweets.json” 
+// and you will get: https://api.twitter.com/1.1/search/tweets.json?q=%40twitterapi
+
+
+
+
+// https://twitter.com/search?q=wentz&src=typd
+// https://api.twitter.com/1.1/search/tweets.json?q=wentz&src=typd
 
 
 /*
